b2454632001cc772053f4d8f577b76e85db4122f
octavianModreanu <octavian.modreanu@gmail.com>
2025-05-16 20:42:43 +0200

    Some minor things preped for next time

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from Categorizer import NaiveBayes
 from collections import Counter
 
 # I want to do a character frequency distribution for each category
+# I should also calculate the average length of the words in each category
 nb = NaiveBayes()
 input_data = "John Doe, 30, Software Engineer, Single"
 
@@ -15,15 +16,20 @@ def letter_count(ctg):
             split_tokens.append(nb.tokenizer(key,split=1))
     for index in split_tokens:
         for j in index:
+                j = j.lower()
                 letters.append(j)
     return dict(Counter(letters))
 
 Name = dict(sorted(letter_count(ctg = "Name").items()))
 Gang = dict(sorted(letter_count(ctg = "Gang").items()))
-print(Counter(Name).most_common(20)[0])
-print(Counter(Gang).most_common(20)[0])
+dict_name = dict(Counter(Name))
+freq_name = {}
+for i in dict_name:
+     freq_name.update({f"{i}": dict_name[i]/Counter(Name).total()})
+print(Counter(Name).total())
+
+#print(Counter(Gang).total())
 """
 So at this point I need to compare distributions
-i need peak, spread,
 
 """
